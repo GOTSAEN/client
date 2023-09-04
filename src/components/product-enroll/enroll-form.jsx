28a82fcb5b9d7a85252c49716fb73f934e740615
gajeong <rlarkwjd127@gmail.com>
2023-09-04 17:55:28 +0900

    title

--- src/components/product-enroll/enroll-form.jsx
+++ src/components/product-enroll/enroll-form.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card, CardHeader } from '../ui/card'
+import { Card, CardHeader, CardTitle } from '../ui/card'
 import { Input } from '../ui/input'
 import { Form, FormField, FormItem } from '../ui/form'
 import {
@@ -30,6 +30,11 @@ export default function EnrollForm() {
 
   return (
     <Card className={cn('p-4 flex flex-col gap-2')}>
+      <CardHeader className={cn('px-1')}>
+        <CardTitle className={cn('text-lg')}>
+          상품 등록
+        </CardTitle>
+      </CardHeader>
       <Input placeholder='상품명' />
       <Input placeholder='모집인원' />
       <div className='flex gap-2'>
