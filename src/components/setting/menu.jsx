99c256ce9c9de6208cef9e80ed68fe145fff5cff
wnsrl7250 <wnsrl7250@naver.com>
2023-09-27 18:35:36 +0900

    비밀번호 변경 UI 개발

--- src/components/setting/menu.jsx
+++ src/components/setting/menu.jsx
@@ -3,150 +3,74 @@ import {
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-} from '@/components/ui/navigation-menu'
-import { cn } from '@/utils/lib'
-import { Link } from 'react-router-dom'
-import React from 'react'
-import { DropdownMenuLabel } from '@radix-ui/react-dropdown-menu'
+} from "@/components/ui/navigation-menu";
+import { cn } from "@/utils/lib";
+import { Link } from "react-router-dom";
+import React from "react";
+import { DropdownMenuLabel } from "@radix-ui/react-dropdown-menu";
 
 export default function Menu() {
   const navigation_styles =
-    'group inline-flex h-10 bg-transparent w-max items-center justify-center rounded-md  rounded-none bg-background px-4 py-2 text-sm font-medium transition-colors border-l-[1px] hover:border-zinc-900   hover:brightness-110 focus:bg-accent focus:text-accent/50 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 w-full'
+    "group inline-flex h-10 bg-transparent w-max items-center justify-center rounded-md  rounded-none bg-background px-4 py-2 text-sm font-medium transition-colors border-l-[1px] hover:border-zinc-900   hover:brightness-110 focus:bg-accent focus:text-accent/50 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 w-full";
   return (
-    <nav className='h-full justify-between w-fit mt-4 pl-2 pr-12 top-0 '>
-      <div className='sticky top-40 max-sm:static'>
+    <nav className="h-full justify-between w-fit mt-4 pl-2 pr-12 top-0 ">
+      <div className="sticky top-40 max-sm:static">
         <NavigationMenu>
-          <NavigationMenuList
-            className={cn('flex flex-col')}
-          >
-            <DropdownMenuLabel className='text-sm font-semibold px-2 py-2 w-full'>
+          <NavigationMenuList className={cn("flex flex-col")}>
+            <DropdownMenuLabel className="text-sm font-semibold px-2 py-2 w-full">
               광고 관리
             </DropdownMenuLabel>
-            <NavigationMenuItem className='w-full'>
+            <NavigationMenuItem className="w-full">
               <Link
-                to='/setting/ads/progress'
-                className='px-0'
+                to="/setting/ads/progress"
+                className="px-0"
                 legacyBehavior
                 passHref
               >
-                <NavigationMenuLink
-                  className={navigation_styles}
-                >
+                <NavigationMenuLink className={navigation_styles}>
                   진행 광고
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
-            <NavigationMenuItem className='w-full'>
-              <Link
-                to='/setting/ads/waiting'
-                legacyBehavior
-                passHref
-              >
-                <NavigationMenuLink
-                  className={navigation_styles}
-                >
+            <NavigationMenuItem className="w-full">
+              <Link to="/setting/ads/waiting" legacyBehavior passHref>
+                <NavigationMenuLink className={navigation_styles}>
                   대기 광고
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
-            <NavigationMenuItem className='w-full'>
-              <Link
-                to='/setting/ads/past'
-                legacyBehavior
-                passHref
-              >
-                <NavigationMenuLink
-                  className={navigation_styles}
-                >
+            <NavigationMenuItem className="w-full">
+              <Link to="/setting/ads/past" legacyBehavior passHref>
+                <NavigationMenuLink className={navigation_styles}>
                   지난 광고
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
-
-            {/* 광고주 광고 관리 임시용 */}
-            <DropdownMenuLabel className='text-sm font-semibold px-2 py-2 w-full'>
-              광고 관리
-            </DropdownMenuLabel>
-            <NavigationMenuItem className='w-full'>
-              <Link
-                to='/setting/partner/ads/progress'
-                className='px-0'
-                legacyBehavior
-                passHref
-              >
-                <NavigationMenuLink
-                  className={navigation_styles}
-                >
-                  진행 광고
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem className='w-full'>
-              <Link
-                to='/setting/partner/ads/waiting'
-                legacyBehavior
-                passHref
-              >
-                <NavigationMenuLink
-                  className={navigation_styles}
-                >
-                  대기 광고
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem className='w-full'>
-              <Link
-                to='/setting/partner/ads/past'
-                legacyBehavior
-                passHref
-              >
-                <NavigationMenuLink
-                  className={navigation_styles}
-                >
-                  지난 광고
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            {/* 정보관리 */}
-            <DropdownMenuLabel className='text-sm font-semibold px-2 py-2 w-full'>
+            <DropdownMenuLabel className="text-sm font-semibold px-2 py-2 w-full">
               정보 관리
             </DropdownMenuLabel>
-            <NavigationMenuItem className='w-full'>
-              <Link
-                to='/setting/ads/past'
-                legacyBehavior
-                passHref
-              >
-                <NavigationMenuLink
-                  className={navigation_styles}
-                >
+            <NavigationMenuItem className="w-full">
+              <Link to="/setting/ads/past" legacyBehavior passHref>
+                <NavigationMenuLink className={navigation_styles}>
                   내 정보
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
-            <NavigationMenuItem className='w-full'>
+            <NavigationMenuItem className="w-full">
               <Link
-                to='/setting/ads/progress'
-                className='px-0'
+                to="/setting/pwchange"
+                className="px-0"
                 legacyBehavior
                 passHref
               >
-                <NavigationMenuLink
-                  className={navigation_styles}
-                >
+                <NavigationMenuLink className={navigation_styles}>
                   패스워드 변경
                 </NavigationMenuLink>
               </Link>
             </NavigationMenuItem>
-            <NavigationMenuItem className='w-full'>
-              <Link
-                to='/setting/ads/waiting'
-                legacyBehavior
-                passHref
-              >
-                <NavigationMenuLink
-                  className={navigation_styles}
-                >
+            <NavigationMenuItem className="w-full">
+              <Link to="/setting/ads/waiting" legacyBehavior passHref>
+                <NavigationMenuLink className={navigation_styles}>
                   회원탈퇴
                 </NavigationMenuLink>
               </Link>
@@ -155,7 +79,7 @@ export default function Menu() {
         </NavigationMenu>
       </div>
     </nav>
-  )
+  );
 }
 
 const ListItem = React.forwardRef(
@@ -166,21 +90,19 @@ const ListItem = React.forwardRef(
           <a
             ref={ref}
             className={cn(
-              'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground ',
+              "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground ",
               className
             )}
             {...props}
           >
-            <div className='text-sm font-medium leading-none'>
-              {title}
-            </div>
-            <p className='line-clamp-2 text-sm leading-snug text-muted-foreground'>
+            <div className="text-sm font-medium leading-none">{title}</div>
+            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
               {children}
             </p>
           </a>
         </NavigationMenuLink>
       </li>
-    )
+    );
   }
-)
-ListItem.displayName = 'ListItem'
+);
+ListItem.displayName = "ListItem";
---
+++ src/pages/setting/PwChange.jsx
@@ -0,0 +1,85 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Tabs, TabsContent } from "@/components/ui/tabs";
+import { cn } from "@/utils/lib";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function PwChange() {
+  const [currentPasswordVisible, setCurrentPasswordVisible] = useState(false);
+  const [newPasswordVisible, setNewPasswordVisible] = useState(false);
+  const [reNewPasswordVisible, setReNewPasswordVisible] = useState(false);
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+
+  const toggleCurrentPasswordVisibility = () => {
+    setCurrentPasswordVisible(!currentPasswordVisible);
+  };
+
+  const toggleNewPasswordVisibility = () => {
+    setNewPasswordVisible(!newPasswordVisible);
+  };
+
+  const toggleReNewPasswordVisibility = () => {
+    setReNewPasswordVisible(!reNewPasswordVisible);
+  };
+
+  return (
+    <div className="flex justify-center items-center mt-10">
+      <Tabs defaultValue="account" className="w-[450px]">
+        <TabsContent value="account">
+          <Card>
+            <CardHeader className={cn("items-center")}>
+              <CardTitle className={cn("mt-5 mb-5")}>
+                - 비밀번호 변경 -
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="flex items-center gap-3">
+                <Input
+                  type={currentPasswordVisible ? "text" : "password"}
+                  id="password"
+                  placeholder="현재 비밀번호"
+                  value={currentPassword}
+                  onChange={(e) => setCurrentPassword(e.target.value)}
+                />
+                <Button onClick={toggleCurrentPasswordVisibility}>
+                  {currentPasswordVisible ? "x" : "x"}
+                </Button>
+              </div>
+              <div className="flex items-center gap-3">
+                <Input
+                  type={newPasswordVisible ? "text" : "password"}
+                  id="password"
+                  placeholder="새 비밀번호"
+                  value={newPassword}
+                  onChange={(e) => setNewPassword(e.target.value)}
+                />
+                <Button onClick={toggleNewPasswordVisibility}>
+                  {newPasswordVisible ? "x" : "x"}
+                </Button>
+              </div>
+              <div className="flex items-center gap-3">
+                <Input
+                  type={reNewPasswordVisible ? "text" : "password"}
+                  id="password"
+                  placeholder="새 비밀번호 확인"
+                  value={rePassword}
+                  onChange={(e) => setRePassword(e.target.value)}
+                />
+                <Button onClick={toggleReNewPasswordVisibility}>
+                  {reNewPasswordVisible ? "x" : "x"}
+                </Button>
+              </div>
+              <Button className={cn("w-full")}>
+                <Link to="/">확인</Link>
+              </Button>
+            </CardContent>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
