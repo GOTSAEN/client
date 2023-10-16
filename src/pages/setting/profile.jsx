573594ad0a2f38d4106672c40e895cf53ec645b3
wnsrl7250 <wnsrl7250@naver.com>
2023-10-16 20:33:53 +0900

    회원정보 조회 api

--- src/pages/setting/profile.jsx
+++ src/pages/setting/profile.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from 'react'
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import { useQuery } from "react-query";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+import { getMember } from "@/api/members";
+import LocationLabel from "@/components/setting/location-label";
 
 export default function Profile() {
   const [inputCount, setInputCount] = useState(2)
@@ -28,6 +31,14 @@ export default function Profile() {
     setInputErrors(newInputErrors)
   }
 
+  const {
+    isLoading,
+    data: memberData,
+    error,
+  } = useQuery(["members"], getMember, {
+    staleTime: 1000 * 60 * 24,
+  });
+
   const handleUpdateProfile = () => {
     const newInputErrors = inputValues.map(
       (value) => value.trim() === ''
@@ -41,85 +52,6 @@ export default function Profile() {
 
   return (
     <>
-<<<<<<< HEAD
-      <div className='flex justify-center flex-col'>
-        <div className='w-full max-w-screen-md '>
-          <h2 className='text-lg font-bold'>내 정보</h2>
-          <p className='text-sm'>
-            This is how others will see you on the site.
-          </p>
-        </div>
-        <div className='flex justify-center flex-col mt-8 gap-5'>
-          <div className='flex justify-center flex-col gap-2'>
-            <h3>이름</h3>
-            <Input></Input>
-            <p className='text-sm'>
-              This is your public display name. It can be
-              your real name or a pseudonym. You can only
-              change this once every 30 days.
-            </p>
-          </div>
-          <div className='flex justify-center flex-col gap-2'>
-            <h3>Email</h3>
-            <Input></Input>
-            <p className='text-sm'>
-              You can manage verified email addresses in
-              your email settings.
-            </p>
-          </div>
-          <div className='flex justify-center flex-col gap-2'>
-            <h3>상호명</h3>
-            <Input></Input>
-            <p className='text-sm'>
-              This is your public display name. It can be
-              your real name or a pseudonym. You can only
-              change this once every 30 days.
-            </p>
-          </div>
-          <div className='flex justify-center flex-col gap-2'>
-            <h3>사업장 주소</h3>
-            <p className='text-sm'>
-              Add links to your workspace, blog, or social
-              media profiles.
-            </p>
-            {Array.from({ length: inputCount }).map(
-              (_, index) => (
-                <div key={index}>
-                  <Input
-                    value={inputValues[index]}
-                    onChange={(e) => {
-                      const newInputValues = [
-                        ...inputValues,
-                      ]
-                      newInputValues[index] = e.target.value
-                      setInputValues(newInputValues)
-                    }}
-                  />
-                  {inputErrors[index] && (
-                    <p className='text-red-500'>
-                      칸이 비어 있습니다.
-                    </p>
-                  )}
-                </div>
-              )
-            )}
-
-            <div className='flex gap-2'>
-              <Button
-                className='w-20'
-                onClick={handleAddInput}
-              >
-                Add
-              </Button>
-              <Button
-                className='w-20'
-                onClick={() =>
-                  handleDeleteInput(inputCount - 1)
-                }
-              >
-                Del
-              </Button>
-=======
       <LocationLabel labels={["정보 관리", "내 정보"]} />
       <div className="flex justify-center flex-col">
         <div className="w-full max-w-screen-md ">
@@ -132,7 +64,6 @@ export default function Profile() {
             <div className="flex justify-center flex-col gap-2">
               <h3>이름</h3>
               <Input></Input>
->>>>>>> 2297407 (회원정보 불러오기)
             </div>
             <div className="flex justify-center flex-col gap-2">
               <h3>Email</h3>
@@ -177,17 +108,13 @@ export default function Profile() {
               <Link to="/">Update Profile</Link>
             </Button>
           </div>
-<<<<<<< HEAD
           <Button
             className='w-32'
             onClick={handleUpdateProfile}
           >
             <Link to='/'>Update Profile</Link>
           </Button>
-        </div>
-=======
         )}
->>>>>>> 2297407 (회원정보 불러오기)
       </div>
     </>
   )
