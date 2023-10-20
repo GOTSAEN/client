d485bcc0228b484295f21fa87033a751d7cd6d9d
gajeong <rlarkwjd127@gmail.com>
2023-10-20 17:47:22 +0900

    alert dark 모드 적용

--- src/components/ui/toast.jsx
+++ src/components/ui/toast.jsx
@@ -1,28 +1,25 @@
-import React from 'react'
+import { useDarkMode } from '@/context/DarkModeContext'
+import React, { useEffect, useState } from 'react'
 
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 export default function AlertToast({ status, message }) {
-  const notify = () =>
-    toast[status](message, {
-      icon: false,
-    })
+  const { darkMode } = useDarkMode()
 
   return (
-    <>
-      <ToastContainer
-        position='bottom-center'
-        autoClose={4000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss={false}
-        draggable
-        pauseOnHover
-        theme='light'
-        limit={1}
-      />
-    </>
+    <ToastContainer
+      toastClassName={'pretendard'}
+      position='bottom-center'
+      autoClose={2000}
+      hideProgressBar={true}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss={false}
+      draggable
+      pauseOnHover
+      theme={darkMode ? 'dark' : 'light'}
+      limit={1}
+    />
   )
 }
