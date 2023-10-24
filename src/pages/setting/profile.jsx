5d81350b57e21751cd4987f8e09296f23ec74252
gajeong <rlarkwjd127@gmail.com>
2023-10-24 16:29:00 +0900

    Profile 충돌 오류 해결

--- src/pages/setting/profile.jsx
+++ src/pages/setting/profile.jsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
-import { useQuery } from "react-query";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
-import { getMember } from "@/api/members";
-import LocationLabel from "@/components/setting/location-label";
+import React, { useState } from 'react'
+import { useQuery } from 'react-query'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { Link } from 'react-router-dom'
+import { getMember } from '@/api/members'
+import LocationLabel from '@/components/setting/location-label'
 
 export default function Profile() {
   const [inputCount, setInputCount] = useState(2)
@@ -35,9 +35,9 @@ export default function Profile() {
     isLoading,
     data: memberData,
     error,
-  } = useQuery(["members"], getMember, {
+  } = useQuery(['members'], getMember, {
     staleTime: 1000 * 60 * 24,
-  });
+  })
 
   const handleUpdateProfile = () => {
     const newInputErrors = inputValues.map(
@@ -52,71 +52,79 @@ export default function Profile() {
 
   return (
     <>
-      <LocationLabel labels={["정보 관리", "내 정보"]} />
-      <div className="flex justify-center flex-col">
-        <div className="w-full max-w-screen-md ">
-          <h2 className="text-lg font-bold">내 정보</h2>
+      <div className='flex justify-center flex-col'>
+        <div className='w-full max-w-screen-md '>
+          <h2 className='text-lg font-bold'>내 정보</h2>
         </div>
         {isLoading && <p>로딩중</p>}
         {error && <p>에러</p>}
         {memberData && (
-          <div className="flex justify-center flex-col mt-8 gap-5">
-            <div className="flex justify-center flex-col gap-2">
-              <h3>이름</h3>
-              <Input></Input>
+          <div className='flex justify-center flex-col mt-8 gap-5'>
+            <div className='flex justify-center flex-col gap-2'>
+              <h3>이메일</h3>
+              <Input
+                value={memberData.email}
+                onChange={(e) => {}}
+              />
             </div>
-            <div className="flex justify-center flex-col gap-2">
-              <h3>Email</h3>
-              <Input value={memberData.email} onChange={(e) => {}} />
-            </div>
-            <div className="flex justify-center flex-col gap-2">
+            <div className='flex justify-center flex-col gap-2'>
               <h3>상호명</h3>
-              <Input value={memberData.businessName} onChange={(e) => {}} />
+              <Input
+                value={memberData.businessName}
+                onChange={(e) => {}}
+              />
             </div>
-            <div className="flex justify-center flex-col gap-2">
+            <div className='flex justify-center flex-col gap-2'>
               <h3>사업장 주소</h3>
 
-              {Array.from({ length: inputCount }).map((_, index) => (
-                <div key={index}>
-                  <Input
-                    value={memberData.businessAddress}
-                    onChange={(e) => {
-                      const newInputValues = [...inputValues];
-                      newInputValues[index] = e.target.value;
-                      setInputValues(newInputValues);
-                    }}
-                  />
-                  {inputErrors[index] && (
-                    <p className="text-red-500">칸이 비어 있습니다.</p>
-                  )}
-                </div>
-              ))}
+              {Array.from({ length: inputCount }).map(
+                (_, index) => (
+                  <div key={index}>
+                    <Input
+                      value={memberData.businessAddress}
+                      onChange={(e) => {
+                        const newInputValues = [
+                          ...inputValues,
+                        ]
+                        newInputValues[index] =
+                          e.target.value
+                        setInputValues(newInputValues)
+                      }}
+                    />
+                    {inputErrors[index] && (
+                      <p className='text-red-500'>
+                        칸이 비어 있습니다.
+                      </p>
+                    )}
+                  </div>
+                )
+              )}
 
-              <div className="flex gap-2">
-                <Button className="w-20" onClick={handleAddInput}>
+              <div className='flex gap-2'>
+                <Button
+                  className='w-20'
+                  onClick={handleAddInput}
+                >
                   Add
                 </Button>
                 <Button
-                  className="w-20"
-                  onClick={() => handleDeleteInput(inputCount - 1)}
+                  className='w-20'
+                  onClick={() =>
+                    handleDeleteInput(inputCount - 1)
+                  }
                 >
                   Del
                 </Button>
               </div>
+
+              <Button
+                className='w-32'
+                onClick={handleUpdateProfile}
+              >
+                <Link to='/'>Update Profile</Link>
+              </Button>
             </div>
-            <Button className="w-32" onClick={handleUpdateProfile}>
-              <Link to="/">Update Profile</Link>
-            </Button>
           </div>
-<<<<<<< HEAD
-          <Button
-            className='w-32'
-            onClick={handleUpdateProfile}
-          >
-            <Link to='/'>Update Profile</Link>
-          </Button>
-=======
->>>>>>> 2297407 (회원정보 불러오기)
         )}
       </div>
     </>
