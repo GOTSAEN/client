74f8cfb1a6aaaf7200977982662e2b9f07435560
gajeong <rlarkwjd127@gmail.com>
2023-12-05 00:12:29 +0900

    refactor/권한 별 메뉴

---
+++ src/data/setting-auth-menu.js
@@ -0,0 +1,50 @@
+
+
+export const MENU_ITEMS = 
+    {
+      youtuber :  [
+          {
+            url : '/setting/ads',
+            label : "광고 관리",
+            menus : [
+              { path: '/waiting', name: '대기 광고' },
+              { path: '/progress', name: '진행 광고' },
+              { path: '/past', name: '종료 광고' },
+              { path: '/bookmark', name: '찜한 광고' },
+  
+            ]
+          },
+          {
+            url : '/setting',
+            label : "정보 관리",
+            menus :[
+              { path: '/profile', name: '회원정보' },
+              { path: '/secession', name: '회원 탈퇴' },
+            ]
+          },
+        ],
+        advertisement :  [
+            {
+              url : '/setting/partner/ads',
+              label : "광고 관리",
+              menus : [
+                { path: '/waiting', name: '등록 광고' },
+                { path: '/progress', name: '진행 광고' },
+                { path: '/past', name: '종료 광고' },
+    
+              ]
+            },
+            {
+              url : '/setting',
+              label : "정보 관리",
+              menus :[
+                { path: '/profile', name: '회원정보' },
+                { path: '/secession', name: '회원 탈퇴' },
+              ]
+            },
+          ]
+    }
+
+  
+
+  
