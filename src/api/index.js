a16c6f5eaaab2cefd9985d25acf1df13ba48b823
gajeong <rlarkwjd127@gmail.com>
2023-12-05 17:36:35 +0900

    chore/axios instance 통일

--- src/api/index.js
+++ src/api/index.js
@@ -2,31 +2,23 @@ import axios, { AxiosInstance } from 'axios';
 import { setInterceptors } from './interceptors';
 axios.defaults.withCredentials = true;
 
-function createInstanceWithAuth(url) {
+function createInstance(url) {
   const instance = axios.create({
     baseURL: `/api/${url}`,
   });
   return setInterceptors(instance);
 }
 
-function createInstanceNoAuth(url) {
-  return axios.create({
-    baseURL: `/api/${url}`,
-  });
-  return setInterceptors(instance);
-}
-
-export const instance = createInstanceWithAuth('');
-export const categories = createInstanceNoAuth('categories');
-export const search = createInstanceNoAuth('search');
-export const authAds = createInstanceWithAuth('advertisement');
-export const ads = createInstanceNoAuth('advertisement');
-
-export const auth = createInstanceNoAuth('auth');
-export const members = createInstanceNoAuth('members');
-export const authMembers = createInstanceWithAuth('members');
-export const youtubers = createInstanceNoAuth('youtubers');
-export const authYoutubers = createInstanceWithAuth('youtubers');
-export const partnerAds = createInstanceWithAuth('members/advertisement');
-export const application = createInstanceWithAuth('applications');
-export const bookmarks = createInstanceWithAuth('bookmarks');
+export const instance = createInstance('');
+export const categories = createInstance('categories');
+export const search = createInstance('search');
+export const authAds = createInstance('advertisement');
+export const ads = createInstance('advertisement');
+export const auth = createInstance('auth');
+export const members = createInstance('members');
+export const authMembers = createInstance('members');
+export const youtubers = createInstance('youtubers');
+export const authYoutubers = createInstance('youtubers');
+export const partnerAds = createInstance('members/advertisement');
+export const application = createInstance('applications');
+export const bookmarks = createInstance('bookmarks');
