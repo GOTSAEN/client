49f9c2d93f931c4a2dcb5d15c43ef1970c9f37cf
gajeong <rlarkwjd127@gmail.com>
2023-12-11 11:58:09 +0900

    feat/element의 요소 바닥 탐지하는 커스텀 훅 생성

---
+++ src/hooks/use-intersection-abserver.jsx
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+
+export const useIntersectionObserver = ({ threshold, hasNextPage, fetchNextPage }) => {
+  const [target, setTarget] = useState();
+
+  const observerCallback = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting && hasNextPage) {
+        fetchNextPage();
+      }
+    });
+  };
+
+  useEffect(() => {
+    if (!target) return;
+    const observer = new IntersectionObserver(observerCallback, {
+      threshold,
+    });
+    console.log('intersection');
+    observer.observe(target);
+
+    return () => observer.unobserve(target);
+  }, [observerCallback, threshold, target]);
+  return { setTarget };
+};
